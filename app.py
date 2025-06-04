7c0f6b921718b05560446715684460f6e0f7fbd9
eogzaz <146135249+eogzaz@users.noreply.github.com>
2025-06-04 04:40:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,59 +4,65 @@ from modules.data_load import *
 from modules.data_cleaning import *
 from modules.data_analysis import *
 
-#------------------Variables importantes--------------------
-paises_latam=['Mexico','Argentina', 'Brazil', 'Chile', 'Colombia','Ecuador', 'Peru', 'Venezuela','Central America','Other South America']
+#------------------------DATOS-------------------------------------
 
-#-------------------------------------------------------------
-
-#------------------Carga de los datos-----------------------
 with open('data.json', 'r') as f:
   Paises_from_json = json.load(f)
 
 Paises = {key: pd.DataFrame(data) for key, data in Paises_from_json.items()}
-#-----------------------------------------------------------
 
+#-----------------------------------------------------------------------
 
-#----------------Codigo streamlit-----------------------------
-st.title("Series de tiempo")
+# ---------- Configuración de la página y estilo del gráfico -----------
+st.set_page_config(
+    page_title="Dashboard Proyecto TalentoTech",
+    layout="wide"
+)
 
+# ------------------- Barra Lateral de Controles (Sidebar) -----------------
+st.sidebar.header("Filtros")
 
-#Seleccion de los pais
-paises_seleccionados = st.multiselect(
-    'Cuales paises te gustaria ver?',
-    list(Paises.keys()),
-    ['Mexico','Argentina', 'Brazil', 'Chile', 'Colombia','Ecuador', 'Peru', 'Venezuela','Central America','Other South America'])
+#Seleccion del pais
+opciones_paises = ['(Elija un pais)','Latinoamerica','Mexico','Argentina', 'Brazil', 'Chile', 'Colombia','Ecuador', 'Peru', 'Venezuela']
+eleccion_pais = st.sidebar.selectbox("Elige un pais para analizar: ", opciones_paises)
 
-
-#Selección de variable a graficar vs el tiempo
-variables = ['(Elija una variable)','Tiempo [años]', 'Generacion total de energia  [TWh]',
-       'Generacion solar [TWh]', 'Generacion eolica [TWh]',
-       'Generacion geotermica-biomasa-otras [TWh]',
-       'Generacion hidroelectrica [TWh]',
-       'Generacion renovable incluyendo hidroelectrica [TWh]',
-       'Generacion no renovable [TWh]', 'Emisiones de CO2 [MTon]',
-       'Comsumo de energia primario [TWh]', 'Comsumo de energia solar [TWh]',
-       'Comsumo de energia eolica [TWh]',
-       'Comsumo de energia hidroelectrica [TWh]',
-       'Comsumo de energia geotermica-biomasa-otras [TWh]',
-       'Comsumo de energia renovable incluyendo hidroelectrica [TWh]',
-       'Comsumo de energia no renovable [TWh]']
-variable_vs_time = st.selectbox("Elige primer variable para graficar: ", variables)
-
-
+#Selección de rango de años
 #Seleccion rango de años
 min_value = 1985
 max_value = 2023
-
-from_year, to_year = st.slider(
+desde_año, hasta_año = st.sidebar.slider(
     'Cuales años te interesan?',
     min_value=min_value,
     max_value=max_value,
     value=[min_value, max_value])
 
-if variable_vs_time=='(Elija una variable)':
-  st.write('Escoge una variable para poder graficar')
-else:
-    fig=grafico_tiempo([Paises[i] for i in paises_seleccionados], variable_vs_time, from_year,to_year,paises_seleccionados)
-    st.pyplot(fig)
+#------------------------------------------------------------------------
+#---------Generación de energia y emisiones de CO2 en el tiempo-----------
+st.title(f"Análisis de generacion Energía y Emisiones de CO2 para {eleccion_pais}")
 
+if eleccion_pais=='(Elija un pais)':
+   st.write('(Escoge un pais para poder graficar)')
+else:
+  fig1=grafico_generacion_y_emision(Paises[eleccion_pais],eleccion_pais,desde_año,hasta_año)
+  st.pyplot(fig1)
+
+
+# --- ----------------Análisis Detallado por Año ----------------------------
+st.title('Matriz energetica')
+
+# Selector para el año específico
+año = st.selectbox(
+    "Selecciona un año para ver la matriz energetica:",
+    options=np.arange(hasta_año,desde_año,-1))
+
+if eleccion_pais=='(Elija un pais)':
+   st.write('(Escoge un pais para poder graficar)')
+else:
+  fig2=grafico_pie(Paises[eleccion_pais],eleccion_pais,año)
+  st.plotly_chart(fig2)
+#---------------------------------------------------------------------------
+if eleccion_pais=='(Elija un pais)':
+   st.write('Escoge un pais para poder graficar')
+else:
+  fig3=grafico_matriz_energetica(Paises[eleccion_pais],eleccion_pais,desde_año,hasta_año)
+  st.pyplot(fig3)
