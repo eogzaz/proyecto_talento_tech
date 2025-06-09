d5f871853d07e9e28ba9b1ddf2ac6f18e86006a0
eogzaz <146135249+eogzaz@users.noreply.github.com>
2025-06-08 23:37:24 -0500

    Update data_load.py

--- modules/data_load.py
+++ modules/data_load.py
@@ -1,8 +1,19 @@
+#-------------------------------------Carga de Datos----------------------------------------------------------------
+#En este script se obtinenen los datos de generación de energia electrica, consumo de energia electrica en 
+#las diferentes fuentes energeticas (renovables y no renovables), y datos de emisiones de CO2. Esto para 
+#diversas regiones y paises del mundo
+#------------------------------------------------------------------------------------------------------------------
+
 import pandas as pd
 import numpy as np
 import requests
 from modules.data_cleaning import obtencion_dataframes
 from modules.data_cleaning import exajoules_to_twh
+
+#----------------------Funcion para cargar datos y primer filtrado-------------------------------------------------
+# Esta función carga los datos de un archivo .xlsx  y lo filtra para obtener los datos necesarios en nuestro
+# analisis, y devuelve una lista donde sus elemntos son dataframes, cada uno de estos dataframes tiene como columnas
+# los paises o regiones que usaremos en el analisis
 
 def Cargar_Datos():
     #Datos obtenidos del proyecto The Energy Institute Statistical Review of World Energy del energy institute (https://www.energyinst.org/statistical-review)
@@ -13,60 +24,65 @@ def Cargar_Datos():
 
     with open(nombre_del_archivo, "wb") as file:
         file.write(response.content)
-
-
-
-    paises_latam=['Mexico','Argentina', 'Brazil', 'Chile', 'Colombia',
-                            'Ecuador', 'Peru', 'Venezuela','Central America',
-                            'Other South America']
+    
+    #Regiones y paises 
+    Regiones_y_paises = ['Argentina','Brazil','Chile','Colombia','Ecuador','Peru','Venezuela','Central America','Other Caribbean','Other South America',
+                         'Canada','Mexico','US','Total North America',
+                         'Total Europe',
+                         'Total CIS',
+                         'Total Middle East',
+                         'Total Africa',
+                         'Total Asia Pacific',
+                         'Total World'
+                        ]
 
     #-----------------------------Datos de emision de CO2------------------------------------------------
-    df_EmisionesCO2 = obtencion_dataframes('Carbon Dioxide from Energy', 'Million tonnes of carbon dioxide',paises=paises_latam)[20:].reset_index(drop=True)
+    df_EmisionesCO2 = obtencion_dataframes('Carbon Dioxide from Energy', 'Million tonnes of carbon dioxide',paises=Regiones_y_paises) [20:].reset_index(drop=True)
 
     #-----------------------------Datos de generación de energía-----------------------------------------
 
     #Generación Total
-    df_electricity_generation = obtencion_dataframes('Electricity Generation - TWh',paises=paises_latam).reset_index(drop=True)
+    df_electricity_generation = obtencion_dataframes('Electricity Generation - TWh',paises=Regiones_y_paises) .reset_index(drop=True)
 
     #Generación Renovable
-    df_solar_generation = obtencion_dataframes('Solar Generation - TWh',paises=paises_latam)[20:].reset_index(drop=True)
-    df_wind_generation  = obtencion_dataframes('Wind Generation - TWh',paises=paises_latam)[20:].reset_index(drop=True)
-    df_hydro_generation = obtencion_dataframes('Hydro Generation - TWh',paises=paises_latam)[20:].reset_index(drop=True)
-    df_GeoBiomassOther  = obtencion_dataframes('Geo Biomass Other - TWh',paises=paises_latam)[20:].reset_index(drop=True)
+    df_solar_generation = obtencion_dataframes('Solar Generation - TWh',paises=Regiones_y_paises) [20:].reset_index(drop=True)
+    df_wind_generation  = obtencion_dataframes('Wind Generation - TWh',paises=Regiones_y_paises) [20:].reset_index(drop=True)
+    df_hydro_generation = obtencion_dataframes('Hydro Generation - TWh',paises=Regiones_y_paises) [20:].reset_index(drop=True)
+    df_GeoBiomassOther  = obtencion_dataframes('Geo Biomass Other - TWh',paises=Regiones_y_paises) [20:].reset_index(drop=True)
 
     #Generación Renovable Total con Hidro
-    df_Suma_Renovables_con_hidro = (df_solar_generation[paises_latam] + df_wind_generation[paises_latam] + df_GeoBiomassOther[paises_latam] + df_hydro_generation[paises_latam])
+    df_Suma_Renovables_con_hidro = (df_solar_generation[Regiones_y_paises]  + df_wind_generation[Regiones_y_paises]  + df_GeoBiomassOther[Regiones_y_paises]  + df_hydro_generation[Regiones_y_paises] )
     df_Suma_Renovables_con_hidro.insert(0,'Años',np.arange(1985.0,2024.0,1))
 
     #Generación Renovable Total sin Hidro
-    df_Suma_Renovables_sin_hidro = (df_solar_generation[paises_latam] + df_wind_generation[paises_latam] + df_GeoBiomassOther[paises_latam])
+    df_Suma_Renovables_sin_hidro = (df_solar_generation[Regiones_y_paises]  + df_wind_generation[Regiones_y_paises]  + df_GeoBiomassOther[Regiones_y_paises] )
     df_Suma_Renovables_sin_hidro.insert(0,'Años',np.arange(1985.0,2024.0,1))
 
     #Generación no Renovable
-    df_no_renovables_generation = (df_electricity_generation[paises_latam] - (df_solar_generation[paises_latam] + df_wind_generation[paises_latam] + df_GeoBiomassOther[paises_latam] + df_hydro_generation[paises_latam]))
+    df_no_renovables_generation = (df_electricity_generation[Regiones_y_paises]  - (df_solar_generation[Regiones_y_paises]  + df_wind_generation[Regiones_y_paises]  + df_GeoBiomassOther[Regiones_y_paises]  + df_hydro_generation[Regiones_y_paises] ))
     df_no_renovables_generation.insert(0,'Años',np.arange(1985.0,2024.0,1))
 
     #------------------------------Datos de comsumo energetico(Se hace la conversion de Exajulios a TWh)-------------------------------------------
 
     #Consumo primario total
-    df_primary_energy_consumption = exajoules_to_twh(obtencion_dataframes('Primary energy cons - EJ','Exajoules',paises=paises_latam)[20:].reset_index(drop=True))
+    df_primary_energy_consumption = exajoules_to_twh(obtencion_dataframes('Primary energy cons - EJ','Exajoules',paises=Regiones_y_paises) [20:].reset_index(drop=True))
 
     #Consumo de energia Renovables
-    df_solar_consumption = exajoules_to_twh(obtencion_dataframes('Solar Consumption - EJ','Exajoules (input-equivalent)',paises=paises_latam)[20:].reset_index(drop=True))
-    df_wind_consumption  = exajoules_to_twh(obtencion_dataframes('Wind Consumption - EJ','Exajoules (input-equivalent)',paises=paises_latam)[20:].reset_index(drop=True))
-    df_hydro_consumption = exajoules_to_twh(obtencion_dataframes('Hydro Consumption - EJ','Exajoules (input-equivalent)*',paises=paises_latam)[20:].reset_index(drop=True))
-    df_GeoBiomassOther_consumption = exajoules_to_twh(obtencion_dataframes('Geo Biomass Other - EJ','Exajoules (input-equivalent)',paises=paises_latam)[20:].reset_index(drop=True))
+    df_solar_consumption = exajoules_to_twh(obtencion_dataframes('Solar Consumption - EJ','Exajoules (input-equivalent)',paises=Regiones_y_paises) [20:].reset_index(drop=True))
+    df_wind_consumption  = exajoules_to_twh(obtencion_dataframes('Wind Consumption - EJ','Exajoules (input-equivalent)',paises=Regiones_y_paises) [20:].reset_index(drop=True))
+    df_hydro_consumption = exajoules_to_twh(obtencion_dataframes('Hydro Consumption - EJ','Exajoules (input-equivalent)*',paises=Regiones_y_paises) [20:].reset_index(drop=True))
+    df_GeoBiomassOther_consumption = exajoules_to_twh(obtencion_dataframes('Geo Biomass Other - EJ','Exajoules (input-equivalent)',paises=Regiones_y_paises) [20:].reset_index(drop=True))
 
     #Consumo Renovable total
-    df_Suma_consumption_Renovables_con_hidro = (df_solar_consumption[paises_latam] + df_wind_consumption[paises_latam]+df_GeoBiomassOther_consumption[paises_latam]+df_hydro_consumption[paises_latam])
+    df_Suma_consumption_Renovables_con_hidro = (df_solar_consumption[Regiones_y_paises]  + df_wind_consumption[Regiones_y_paises] +df_GeoBiomassOther_consumption[Regiones_y_paises] +df_hydro_consumption[Regiones_y_paises] )
     df_Suma_consumption_Renovables_con_hidro.insert(0,'Años',np.arange(1985.0,2024.0,1))
 
     #Consumo Renovable total sin hidro
-    df_Suma_consumption_Renovables_sin_hidro = (df_solar_consumption[paises_latam] + df_wind_consumption[paises_latam]+df_GeoBiomassOther_consumption[paises_latam])
+    df_Suma_consumption_Renovables_sin_hidro = (df_solar_consumption[Regiones_y_paises]  + df_wind_consumption[Regiones_y_paises] +df_GeoBiomassOther_consumption[Regiones_y_paises] )
     df_Suma_consumption_Renovables_sin_hidro.insert(0,'Años',np.arange(1985.0,2024.0,1))
 
     #Consumo No Renovable total
-    df_no_renovables_consumption = (df_primary_energy_consumption[paises_latam] - (df_solar_consumption[paises_latam] + df_wind_consumption[paises_latam]+df_GeoBiomassOther_consumption[paises_latam]+df_hydro_consumption[paises_latam]))
+    df_no_renovables_consumption = (df_primary_energy_consumption[Regiones_y_paises]  - (df_solar_consumption[Regiones_y_paises]  + df_wind_consumption[Regiones_y_paises] +df_GeoBiomassOther_consumption[Regiones_y_paises] +df_hydro_consumption[Regiones_y_paises] ))
     df_no_renovables_consumption.insert(0,'Años',np.arange(1985.0,2024.0,1))
 
     datos=[df_electricity_generation,
