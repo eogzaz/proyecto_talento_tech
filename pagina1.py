9bb5bc6d98f1a0d8751fd59c1975c64d41c80a88
eogzaz <146135249+eogzaz@users.noreply.github.com>
2025-06-09 11:05:07 -0500

    Update pagina1.py

--- pagina1.py
+++ pagina1.py
@@ -16,29 +16,9 @@ Paises = {key: pd.DataFrame(data) for key, data in Paises_from_json.items()}
 st.set_page_config(layout="wide")
 
 #Titulo, se usa st.markdown para mayor estetica,
-#la otra opcion es: st.title(f"Evolución de la generación de energía electrica y Emisiones de CO2")
-st.markdown("""
-        <style>
-        .title-container {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100px; /* puedes reducirlo si quieres más arriba */
-            margin-top: -40px; /* sube el título */
-            margin-bottom: 20px;
-        }
-        .title-text {
-            font-size: 40px;
-            font-weight: bold;
-            color: white;
-            text-align: center;
-        }
-        </style>
+#la otra opcion es: 
+st.title(f"Evolución de la generación de energía electrica y Emisiones de CO2")
 
-        <div class="title-container">
-            <div class="title-text">Evolución de la generación de energía eléctrica y de las emisiones de CO₂</div>
-        </div>
-            """, unsafe_allow_html=True)
 #-----------------------------
 
 #Pendiente comentar este codigo......
