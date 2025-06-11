58474a31cd5316374488884651e29a4310dcdd71
eogzaz <146135249+eogzaz@users.noreply.github.com>
2025-06-11 15:14:10 -0500

    Update pagina2.py

--- pagina2.py
+++ pagina2.py
@@ -65,8 +65,8 @@ with col1:
         #st.pyplot(fig2)
 
 
-  fig3=grafico_tiempo(Paises['Colombia'],1985,2023,'Colombia')
-  st.pyplot(fig3)
+        fig3=grafico_tiempo(Paises['Colombia'],1985,2023,'Colombia')
+        st.pyplot(fig3)
   
   #fig4=grafico_tiempo(Paises['Colombia'],1985,2023,'Latinoamerica')
   
