90dfdd3182b07c0b6ca5a175450ce9052e7939f9
eogzaz <146135249+eogzaz@users.noreply.github.com>
2025-06-11 12:20:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,9 @@ import streamlit as st
 
 def paginaInicio():
     st.set_page_config(layout="wide")
-    st.title("Página de inicio")
-
+    st.title("Diagnostico De la transición energetica y sus efectos en las emisiones de CO2 en Colombia y Latam")
+    st.write()
+    
 pg = st.navigation([
     st.Page(paginaInicio, title="Inicio"),
     st.Page("pagina1.py", title="Energía electrica y Emisiones de C02"),
