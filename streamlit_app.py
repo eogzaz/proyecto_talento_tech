81bdf55a0d4e75e97a6e95b338db13cbd2f54545
eogzaz <146135249+eogzaz@users.noreply.github.com>
2025-06-11 12:22:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,29 @@ import streamlit as st
 
 def paginaInicio():
     st.set_page_config(layout="wide")
-    st.title("Diagnostico De la transición energetica y sus efectos en las emisiones de CO2 en Colombia y Latam")
+    st.markdown("""
+        <style>
+        .title-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100px; /* puedes reducirlo si quieres más arriba */
+            margin-top: -40px; /* sube el título */
+            margin-bottom: 20px;
+        }
+        .title-text {
+            font-size: 40px;
+            font-weight: bold;
+            color: white;
+            text-align: center;
+        }
+        </style>
+
+        <div class="title-container">
+            <div class="title-text">Diagnostico De la transición energetica y sus efectos en las emisiones de CO2 en Colombia y Latam</div>
+        </div>
+            """, unsafe_allow_html=True)
+    #st.title("Diagnostico De la transición energetica y sus efectos en las emisiones de CO2 en Colombia y Latam")
     st.write()
     
 pg = st.navigation([
