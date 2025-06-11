38cc11a0366ada04c6c772383d1e82d90706d6d4
eogzaz <146135249+eogzaz@users.noreply.github.com>
2025-06-11 14:16:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ pg = st.navigation([
     st.Page("paginaInicio.py", title="Inicio"),
     st.Page("pagina1.py", title="Energía electrica y Emisiones de C02"),
     st.Page("pagina2.py", title="Mix Eléctrico"),
-    st.Page("pagina3.py", title="Predicciones")
+    st.Page("streamlit.py", title="Predicciones")
 ])
 pg.run()
