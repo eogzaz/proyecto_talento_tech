982bdba9d6c13dc157fff079497e30ab7cbaf757
eogzaz <146135249+eogzaz@users.noreply.github.com>
2025-06-11 12:41:29 -0500

    Update paginaInicio.py

--- paginaInicio.py
+++ paginaInicio.py
@@ -21,6 +21,16 @@ st.markdown("""
         <div class="title-container">
             <div class="title-text">Diagnostico De la transición energetica y sus efectos en las emisiones de  CO₂ en Colombia y Latam</div>
         </div>
+        <div class="parrafo">
+            <p>Este trabajo aborda el impacto de la transición energética eléctrica en las emisiones de CO₂ 
+en Colombia, con un enfoque hacia fuentes de energía renovable. Se analiza cómo el país,
+históricamente dependiente de la generación hídrica, enfrenta retos relacionados con la variabilidad 
+climática y la necesidad de diversificación energética. 
+El estudio diagnostica y proyecta el cumplimiento los avances normativos y la importancia de integrar 
+fuentes renovables,como solar, eólica, geotermica y biomasa, para lograr un sistema más sostenible, 
+resiliente y alineado con los Objetivos de Desarrollo Sostenible, 
+especialmente el ODS 7 y el ODS 13.</p>   
+        </div>
             """, unsafe_allow_html=True)
 #st.title("Diagnostico De la transición energetica y sus efectos en las emisiones de CO2 en Colombia y Latam")
 
